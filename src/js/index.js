9fa9595eda4f5c1b14d5c3f616b5814c74a77915
inhamostova <inha.mostova@gmail.com>
2026-07-17 21:38:09 +0300

    add infinity scroll

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,7 @@ const searchForm = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 const infoMsg = document.querySelector('.info-msg');
+const target = document.querySelector('.js-target');
 
 const lightbox = new SimpleLightbox('.gallery a');
 
@@ -19,27 +20,69 @@ let currentQuery = null;
 let page = 1;
 
 searchForm.addEventListener('submit', onSearchSubmit);
-loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+// loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 
-function onLoadMoreBtnClick() {
-  fetchImages(currentQuery, page, PER_PAGE)
-    .then(data => {
-      if (data.totalHits < page * PER_PAGE) {
-        hideEl(loadMoreBtn);
-        showEl(infoMsg);
-      }
-      page += 1;
-      gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
-      smoothScrollAfterLoadImages(gallery.firstElementChild);
-      lightbox.refresh();
-    })
-    .catch(err => console.error(err));
-}
+const options = {
+  root: null,
+  rootMargin: '600px',
+  scrollMargin: '0px',
+  threshold: 1.0,
+};
+
+const callbackObserver = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      fetchImages(currentQuery, page, PER_PAGE)
+        .then(data => {
+          if (data.totalHits < page * PER_PAGE) {
+            // hideEl(loadMoreBtn);
+            showEl(infoMsg);
+            observer.unobserve(target);
+          }
+          page += 1;
+          gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+          lightbox.refresh();
+          smoothScrollAfterLoadImages(gallery.firstElementChild);
+        })
+        .catch(err => console.error(err));
+    }
+  });
+
+  // fetchImages(currentQuery, page, PER_PAGE)
+  //   .then(data => {
+  //     if (data.totalHits < page * PER_PAGE) {
+  //       hideEl(loadMoreBtn);
+  //       showEl(infoMsg);
+  //     }
+  //     page += 1;
+  //     gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+  //     smoothScrollAfterLoadImages(gallery.firstElementChild);
+  //     lightbox.refresh();
+  //   })
+  //   .catch(err => console.error(err));
+};
+
+const observer = new IntersectionObserver(callbackObserver, options);
+
+// function onLoadMoreBtnClick() {
+//   fetchImages(currentQuery, page, PER_PAGE)
+//     .then(data => {
+//       if (data.totalHits < page * PER_PAGE) {
+//         hideEl(loadMoreBtn);
+//         showEl(infoMsg);
+//       }
+//       page += 1;
+//       gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+//       smoothScrollAfterLoadImages(gallery.firstElementChild);
+//       lightbox.refresh();
+//     })
+//     .catch(err => console.error(err));
+// }
 
 function onSearchSubmit(evt) {
   page = 1;
   evt.preventDefault();
-  hideEl(loadMoreBtn);
+  //   hideEl(loadMoreBtn);
   hideEl(infoMsg);
 
   const {
@@ -64,8 +107,9 @@ function onSearchSubmit(evt) {
         return;
       }
       Notify.success(`Hooray! We found ${data.totalHits} images.`);
-      showEl(loadMoreBtn);
+      //   showEl(loadMoreBtn);
       gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+      observer.observe(target);
 
       smoothScrollAfterLoadImages(searchForm);
 
@@ -73,6 +117,6 @@ function onSearchSubmit(evt) {
     })
     .catch(err => {
       console.error(err.message);
-      hideEl(loadMoreBtn);
+      //   hideEl(loadMoreBtn);
     });
 }
